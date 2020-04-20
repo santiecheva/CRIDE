82791055301dc009fba30c061d32ed8cf06377da
Santiago Echeverri <santiecheva@gmail.com>
2020-04-20 17:02:43 -0500

    app usuarios

---
+++ cride/users/apps.py
@@ -0,0 +1,13 @@
+""" 
+Users APP 
+"""
+
+#Django
+
+from django.apps import AppConfig
+
+class UsersAppsConfig(AppConfig):
+	"""User app config """
+
+	name = 'cride.users'
+	verbose_name = 'Users'
