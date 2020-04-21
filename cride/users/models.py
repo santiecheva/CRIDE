cda28bc45f9a845524f9300422c5460c91505cde
Santiago Echeverri <santiecheva@gmail.com>
2020-04-21 16:08:10 -0500

    app usuarios

--- cride/users/models.py
+++ cride/users/models.py
@@ -32,7 +32,7 @@ class User(CRideModel,AbstractUser):
 	is_client = models.BooleanField(
 		'client status',
 		default = True,
-		help_text=('Help easily distinguis users and perform quer. '
+		help_text=('Help easily distinguis users and perform queriess. '
 			'Clients are the main type of user'
 		)
 	)
