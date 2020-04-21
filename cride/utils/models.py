6ffb9f77786cf8c61f8b0700d801895bf49ec753
Santiago Echeverri <santiecheva@gmail.com>
2020-04-20 22:53:35 -0500

    app usuarios

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -7,13 +7,13 @@ from django.db import models
 class CRideModel(models.Model):
 
 	created = models.DateTimeField(
-		'created at'
+		'created at',
 		auto_now_add= True,
 		help_text = 'Date time on which the object was created'
 	 	)
 
 	modified = models.DateTimeField(
-		'modified at'
+		'modified at',
 		auto_now = True,
 		help_text = 'Date time on which the object was last modified'
 	 	)
