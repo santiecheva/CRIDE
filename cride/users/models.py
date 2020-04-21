81f27bd33952120e294986a4841b8fc3e4e5a056
Santiago Echeverri <santiecheva@gmail.com>
2020-04-20 21:39:06 -0500

    app usuarios

--- cride/users/models.py
+++ cride/users/models.py
@@ -3,14 +3,14 @@
 # Django
 
 from django.db import models
-from django.contrib.auth.models import AbstractUsers
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
 
 # utilities
 from cride.utils.models import CRideModel
 
-class User(CRideModel,AbstractUsers):
+class User(CRideModel,AbstractUser):
 """  
 Extend from Django's Abstract user models, change the username field to email 
 and add some extra field 
