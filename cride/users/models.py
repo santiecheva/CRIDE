689df329e2c23587216b57bafb8bf24d83e3b6d5
Santiago Echeverri <santiecheva@gmail.com>
2020-04-21 09:51:34 -0500

    app usuarios

--- cride/users/models.py
+++ cride/users/models.py
@@ -24,7 +24,7 @@ class User(CRideModel,AbstractUser):
 		regex =r'\+?1?\d{9,15}$',
 		message = "Phone number must be entered in the format: +99999999. Up to 15 digits allowed.")
 
-	phone_number = models.CharField(validators=[phone_regex], max_lenght = 17, blank = True)
+	phone_number = models.CharField(validators=[phone_regex], max_length = 17, blank = True)
 
 	USERNAME_FIELd = 'email'
 	REQUIRED_FIELDS = ['username','first_name','last_name']
