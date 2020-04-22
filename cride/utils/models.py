b01bb5018df2274ca95332745b0154b76fecd4cc
Santiago Echeverri <santiecheva@gmail.com>
2020-04-22 15:51:20 -0500

    app usuarios

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -9,7 +9,7 @@ class CRideModel(models.Model):
     created = models.DateTimeField(
         'created at',
         auto_now_add= True,
-        help_text = 'Date time on which the object was creadooooooooooooa rrrr SSSS'
+        help_text = 'Date time on which the object was creadooooooooooooa rrrr SSS'
         )
 
     modified = models.DateTimeField(
